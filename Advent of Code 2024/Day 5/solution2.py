8f0d80119cd2e31d3feddb313fff2530e79d39ff
Barca Koseoglu <barcakoseoglu@gmail.com>
2024-12-05 11:33:50 -0600

    Update solution2.py

--- Advent of Code 2024/Day 5/solution2.py
+++ Advent of Code 2024/Day 5/solution2.py
@@ -14,7 +14,6 @@ before = {}
 p1 = 0
 while example[p1] != '':
     example[p1] = example[p1].split('|')
-    example[p1] = [int(i) for i in example[p1]]
 
     if example[p1][0] in before:
         before[example[p1][0]].append(example[p1][1])
@@ -26,7 +25,6 @@ while example[p1] != '':
 p1 += 1
 while p1 != len(example):
     example[p1] = example[p1].split(',')
-    example[p1] = [int(i) for i in example[p1]]
 
     for i in range(len(example[p1])-1):
         if example[p1][i] not in before:
@@ -46,11 +44,76 @@ while p1 != len(example):
             break
     
     if not valid:
-        example[p1].sort()
-        example[p1] = example[p1][::-1]
-        print(example[p1])
-        ans += example[p1][len(example[p1])//2]
+        make = []
+        count = {}
+        for i in example[p1]:
+            count[i] = 0
+            if i not in before:
+                continue
+            for j in example[p1]:
+                if j in before[i]:
+                    count[i] += 1
+        count = {value: key for key, value in count.items()}
+        for i in range(len(example[p1])-1, -1, -1):
+            make.append(count[i])
+        ans += int(make[len(make)//2])
+    p1 += 1
 
+print(ans)
+
+#input
+ans = 0
+
+inp = inp.replace('\n', ';')
+inp = inp.split(';')
+
+before = {}
+p1 = 0
+while inp[p1] != '':
+    inp[p1] = inp[p1].split('|')
+
+    if inp[p1][0] in before:
+        before[inp[p1][0]].append(inp[p1][1])
+    else:
+        before[inp[p1][0]] = [inp[p1][1]]
+
+    p1 += 1
+
+p1 += 1
+while p1 != len(inp):
+    inp[p1] = inp[p1].split(',')
+
+    for i in range(len(inp[p1])-1):
+        if inp[p1][i] not in before:
+            valid = False
+            break
+
+        check = before[inp[p1][i]]
+        
+        valid = True
+
+        for j in range(i+1, len(inp[p1])):
+            if inp[p1][j] not in check:
+                valid = False
+                break
+
+        if not valid:
+            break
+    
+    if not valid:
+        make = []
+        count = {}
+        for i in inp[p1]:
+            count[i] = 0
+            if i not in before:
+                continue
+            for j in inp[p1]:
+                if j in before[i]:
+                    count[i] += 1
+        count = {value: key for key, value in count.items()}
+        for i in range(len(inp[p1])-1, -1, -1):
+            make.append(count[i])
+        ans += int(make[len(make)//2])
     p1 += 1
 
 print(ans)
